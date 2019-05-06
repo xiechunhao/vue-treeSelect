9f1b38eb644b25f78571b38ddf0c14e9d125c40e
xiechunhao <70255403@qq.com>
2019-05-07 01:04:58 +0800

    vue树状下拉组件

---
+++ README.md
@@ -0,0 +1,27 @@
+# vue-treeSelect
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+
+
+### 组件效果如下:
+
+![](src\assets\el-select-tree.gif)
+
+
+
+​	**如果能满足您的需求，麻烦爹妈给一个 星星！！！**
+
---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+    <h1>{{`基于Element-UI组件改造的树形选择器`}}:</h1>
+    <!-- 调用树形下拉框组件 -->
+    <SelectTree
+      :props="props"
+      :options="optionData"
+      :value="valueId"
+      :clearable="isClearable"
+      :accordion="isAccordion"
+      @getValue="getValue($event)"
+    />
+    ID为：{{valueId}}
+  </div>
+</template>
+
+<script>
+import SelectTree from "./components/treeSelect.vue";
+export default {
+  name: "app",
+  components: {
+    SelectTree
+  },
+  data() {
+    return {
+      isClearable: true, // 可清空（可选）
+      isAccordion: true, // 可收起（可选）
+      valueId: 1, // 初始ID（可选）
+      props: {
+        // 配置项（必选）
+        value: "id",
+        label: "name",
+        children: "children"
+        // disabled:true
+      },
+      // 选项列表（必选）
+      list: [
+        { id: 1, parentId: 0, name: "一级菜单A", rank: 1 },
+        { id: 2, parentId: 0, name: "一级菜单B", rank: 1 },
+        { id: 3, parentId: 0, name: "一级菜单C", rank: 1 },
+        { id: 4, parentId: 1, name: "二级菜单A-A", rank: 2 },
+        { id: 5, parentId: 1, name: "二级菜单A-B", rank: 2 },
+        { id: 6, parentId: 2, name: "二级菜单B-A", rank: 2 },
+        { id: 7, parentId: 4, name: "三级菜单A-A-A", rank: 3 },
+        { id: 8, parentId: 7, name: "四级菜单A-A-A-A", rank: 4 },
+        { id: 9, parentId: 0, name: "一级菜单C", rank: 1 },
+        { id: 10, parentId: 0, name: "一级菜单end", rank: 1 }
+      ],
+    };
+  },
+  computed: {
+    /* 转树形数据 */
+    optionData() {
+      let cloneData = JSON.parse(JSON.stringify(this.list)); // 对源数据深度克隆
+      return cloneData.filter(father => {
+        // 循环所有项，并添加children属性
+        let branchArr = cloneData.filter(child => father.id == child.parentId); // 返回每一项的子级数组
+        branchArr.length > 0 ? (father.children = branchArr) : ""; //给父级添加一个children属性，并赋值
+        return father.parentId == 0; //返回第一层
+      });
+    }
+  },
+  methods: {
+    // 取值
+    getValue(value) {
+      this.valueId = value;
+      console.log(this.valueId);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
