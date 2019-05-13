064838a307dae64f3b881d819bb1619d1f43d939
xiechunhao <70255403@qq.com>
2019-05-14 02:47:32 +0800

    动态数据

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@
 
 <script>
 import SelectTree from "./components/treeSelect.vue";
+import treeJson from './api/tree.json';
 export default {
   name: "app",
   components: {
@@ -35,18 +36,26 @@ export default {
       },
       // 选项列表（必选）
       list: [
-        { id: 1, parentId: 0, name: "一级菜单A", rank: 1 },
-        { id: 2, parentId: 0, name: "一级菜单B", rank: 1 },
-        { id: 3, parentId: 0, name: "一级菜单C", rank: 1 },
-        { id: 4, parentId: 1, name: "二级菜单A-A", rank: 2 },
-        { id: 5, parentId: 1, name: "二级菜单A-B", rank: 2 },
-        { id: 6, parentId: 2, name: "二级菜单B-A", rank: 2 },
-        { id: 7, parentId: 4, name: "三级菜单A-A-A", rank: 3 },
-        { id: 8, parentId: 7, name: "四级菜单A-A-A-A", rank: 4 },
-        { id: 9, parentId: 0, name: "一级菜单C", rank: 1 },
-        { id: 10, parentId: 0, name: "一级菜单end", rank: 1 }
+      //   { id: 1, parentId: 0, name: "一级菜单A", rank: 1 },
+      //   { id: 2, parentId: 0, name: "一级菜单B", rank: 1 },
+      //   { id: 3, parentId: 0, name: "一级菜单C", rank: 1 },
+      //   { id: 4, parentId: 1, name: "二级菜单A-A", rank: 2 },
+      //   { id: 5, parentId: 1, name: "二级菜单A-B", rank: 2 },
+      //   { id: 6, parentId: 2, name: "二级菜单B-A", rank: 2 },
+      //   { id: 7, parentId: 4, name: "三级菜单A-A-A", rank: 3 },
+      //   { id: 8, parentId: 7, name: "四级菜单A-A-A-A", rank: 4 },
+      //   { id: 9, parentId: 0, name: "一级菜单C", rank: 1 },
+      //   { id: 10, parentId: 0, name: "一级菜单end", rank: 1 }
       ],
     };
+  },
+  created(){
+    this.getNodeTree(treeJson);
+    let obj = {}
+    this.list = this.list.reduce((item, next) => {
+      obj[next.id] ? '' : obj[next.id] = true && item.push(next)
+      return item
+    }, [])
   },
   computed: {
     /* 转树形数据 */
@@ -65,7 +74,22 @@ export default {
     getValue(value) {
       this.valueId = value;
       console.log(this.valueId);
-    }
+    },
+    getNodeTree(tree){
+      for(var i in tree){
+        if(typeof tree[i] == 'object'){
+          this.getNodeTree(tree[i])
+        }else{
+          this.list.push(
+            {
+              "id":tree["id"],
+              "parentId":tree["parentId"],
+              "name":tree["goodsTypeName"]
+            }
+          )
+        }
+      }
+    },
   }
 };
 </script>
